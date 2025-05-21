b3db989f933908d00cb38be28cca5b64a6df446f
Chill Guy <chill.guy.coding@gmail.com>
2025-05-21 19:10:48 -0400

    Added basic palindrome functionality to module

--- lib/rossdan_palindrome.rb
+++ lib/rossdan_palindrome.rb
@@ -5,4 +5,18 @@ require_relative "rossdan_palindrome/version"
 module RossdanPalindrome
   class Error < StandardError; end
   # Your code goes here...
+  
+  def palindrome?
+    processed_content == processed_content.reverse
+  end
+
+  private
+    
+    def processed_content
+      self.downcase
+    end
+end
+
+class String
+  include RossdanPalindrome
 end
--- test/test_rossdan_palindrome.rb
+++ test/test_rossdan_palindrome.rb
@@ -3,11 +3,11 @@
 require "test_helper"
 
 class TestRossdanPalindrome < Minitest::Test
-  def test_that_it_has_a_version_number
-    refute_nil ::RossdanPalindrome::VERSION
+  def test_non_palindrome
+    assert !"apple".palindrome?
   end
 
-  def test_it_does_something_useful
-    assert true
+  def test_palindrome
+    assert "racecar".palindrome?
   end
 end
