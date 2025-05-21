e1145c3d5b57bc571deae42fefacd9230198e4e1
Chill Guy <chill.guy.coding@gmail.com>
2025-05-21 18:36:04 -0400

    initial repo

---
+++ lib/rossdan_palindrome.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "rossdan_palindrome/version"
+
+module RossdanPalindrome
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ test/test_rossdan_palindrome.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+class TestRossdanPalindrome < Minitest::Test
+  def test_that_it_has_a_version_number
+    refute_nil ::RossdanPalindrome::VERSION
+  end
+
+  def test_it_does_something_useful
+    assert false
+  end
+end
