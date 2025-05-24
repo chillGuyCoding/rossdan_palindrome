9df02b5532fb4c7935796e2f775db676f90d89a8
Chill Guy <chill.guy.coding@gmail.com>
2025-05-24 00:37:38 -0400

    Use select statement instead of manual .each loop for selecting alpha chars

--- lib/rossdan_palindrome.rb
+++ lib/rossdan_palindrome.rb
@@ -11,11 +11,7 @@ module RossdanPalindrome
   end
 
   def letters
-    temp = []
-    chars.each do |char|
-      temp << char if char.match?(/[a-zA-Z]/)
-    end
-    temp.join
+    self.chars.select { |c| c.match?(/[a-z]/i) }.join
   end
 
   private
